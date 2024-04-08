1b18abdc54fd7309503c07417dc620d2981c0879
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-04-08 13:34:16 +0530

    linked list

--- src/linkedlist_kunal/Main.java
+++ src/linkedlist_kunal/Main.java
@@ -2,12 +2,46 @@ package linkedlist_kunal;
 
 public class Main {
     public static void main(String[] args) {
+        /* // Singly linked list
         SinglyLinkedList list = new SinglyLinkedList();
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(17);
+        list.insert(100,4);
+        list.insertLast(99);
+        list.display();
 
+        System.out.println(list.deleteFirst());
+        list.display();
+
+        System.out.println(list.deleteLast());
+        list.display();
+
+        System.out.println(list.delete(2));
+        list.display();
+
+        System.out.println(list.find(2));
+        System.out.println(list.get(1));
+        */
+
+        /*
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertLast(99);
+        list.insert(99, 5);
+        
+        list.display();
+        */
+
+        CircularLinkedList list = new CircularLinkedList();
+        list.insert(13);
+        list.insert(23);
+        list.insert(33);
+        list.delete(23);
         list.display();
     }
 }
--- src/linkedlist_kunal/SinglyLinkedList.java
+++ src/linkedlist_kunal/SinglyLinkedList.java
@@ -36,6 +36,86 @@ public class SinglyLinkedList {
         size++;
     }
 
+    public void insert(int val, int index) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head; // start checking from head
+        for (int i = 1; i < index; i++) { // if index is 3 for to 2
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next); // temp.next means next element
+        temp.next = node; // now temp.next will be the new created node
+    }
+
+    public int delete(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        }
+
+        if (index == size - 1) {
+            return deleteLast();
+        }
+
+        Node prev = get (index - 1);
+        int val = prev.next.value;
+
+        prev.next = prev.next.next;
+
+        return val;
+    }
+
+    public Node find (int value) { // get reference pointer of particular nodes
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null; // means did find the value
+    }
+
+    public Node get (int index) { // get reference pointer of particular nodes
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size - 2); // second last element
+        int val = tail.value;
+
+        tail = secondLast;
+        tail.next = null;
+
+        return val;
+    }
+
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+
+        if(head == null) {
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
     public void display() {
         Node temp = head;
         while (temp != null) {
