04d9f3da6faf3251e2435e0d38e5e04bce4b2a29
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-05-09 22:54:28 +0530

    queue

---
+++ src/queue_kunal/CircularQueue.java
@@ -0,0 +1,73 @@
+package queue_kunal;
+
+public class CircularQueue {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    protected int end = 0;
+    protected int front = 0;
+    protected int size = 0;
+
+    public CircularQueue() {
+        this(DEFAULT_SIZE);
+    }
+
+    public CircularQueue(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isFull() {
+        return size == data.length - 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean insert(int item) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+            return false;
+        }
+        data[end++] = item;
+
+        end = end % data.length;
+        size++;
+
+        return true;
+    }
+
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is full");
+        }
+        int removed = data[front++];
+
+        front = front % data.length;
+        size--;
+
+        return removed;
+    }
+
+    public int front() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+        return data[front];
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Empty");
+            return;
+        }
+
+        int i = front;
+        do {
+            System.out.print(data[i] + " <- ");
+            i++;
+            i = i % data.length;
+        } while (i != end);
+        System.out.println("Cycle continues");
+    }
+}
--- src/queue_kunal/QueueMain.java
+++ src/queue_kunal/QueueMain.java
@@ -2,17 +2,31 @@ package queue_kunal;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception {
-        CustomQueue queue = new CustomQueue(5);
-        queue.insert(1);
-        queue.insert(2);
-        queue.insert(3);
-        queue.insert(4);
-        queue.insert(5);
+//        CustomQueue queue = new CustomQueue(5);
+//        queue.insert(1);
+//        queue.insert(2);
+//        queue.insert(3);
+//        queue.insert(4);
+//        queue.insert(5);
+//
+//        queue.display();
+//
+//        queue.remove();
+//        System.out.println("After removing: ");
+//        queue.display();
 
-        queue.display();
-        
-        queue.remove();
+        CircularQueue queue1 = new CircularQueue(5);
+        queue1.insert(1);
+        queue1.insert(2);
+        queue1.insert(3);
+        queue1.insert(4);
+        queue1.insert(5);
+
+        queue1.display();
+
+        queue1.remove();
         System.out.println("After removing: ");
-        queue.display();
+        queue1.insert(6);
+        queue1.display();
     }
 }
