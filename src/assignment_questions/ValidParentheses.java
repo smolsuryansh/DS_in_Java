863d05b3b5dcff1a8f0717d94564878904233aa4
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-05-14 19:42:19 +0530

    assignments

---
+++ src/assignment_questions/ValidParentheses.java
@@ -0,0 +1,34 @@
+package assignment_questions;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (ch == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+                }
+
+                if (ch == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+
+                if (ch == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
