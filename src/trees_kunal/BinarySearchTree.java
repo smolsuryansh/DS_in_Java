442889ff65ef596b80921400abc0d9e383419e4c
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-07-13 23:29:01 +0530

    binary search tree implementation

--- src/trees_kunal/BinarySearchTree.java
+++ src/trees_kunal/BinarySearchTree.java
@@ -47,7 +47,7 @@ public class BinarySearchTree {
     }
 
     public void insert(int value) {
-
+        root = insert(value, root);
     }
 
     private Node insert(int value, Node node) {
@@ -62,5 +62,56 @@ public class BinarySearchTree {
         if (value > node.value) {
             node.right = insert(value, node.right);
         }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        return node;
+    }
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void display1() {
+        display1(this.root, "Root node: ");
+    }
+
+    private void display1(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+
+        display1(node.left, "Left child of " + node.value + ": ");
+        display1(node.right, "Right child of " + node.value + ": ");
+    }
+
+    public void populate (int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populateSorted(int[] nums) {
+        populateSorted(nums, 0, nums.length);
+    }
+
+    private void populateSorted (int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = (start + end) / 2;
+
+        this.insert(nums[mid]);
+        populateSorted(nums, start, mid);
+        populateSorted(nums, mid + 1, end);
     }
 }
--- src/trees_kunal/Main.java
+++ src/trees_kunal/Main.java
@@ -4,9 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(sc);
-        tree.prettyDisplay();
+//        Scanner sc = new Scanner(System.in);
+//        BinaryTree tree = new BinaryTree();
+//        tree.populate(sc);
+//        tree.prettyDisplay();
+
+        BinarySearchTree bst = new BinarySearchTree();
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        bst.populateSorted(nums);
+        bst.display1();
     }
 }
