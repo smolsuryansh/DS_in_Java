6a0f4fbed80bd3c3e14560a8995cdc3c28579c38
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-07-22 23:53:31 +0530

    traversals in trees

--- src/trees_kunal/BinarySearchTree.java
+++ src/trees_kunal/BinarySearchTree.java
@@ -114,4 +114,43 @@ public class BinarySearchTree {
         populateSorted(nums, start, mid);
         populateSorted(nums, mid + 1, end);
     }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
 }
