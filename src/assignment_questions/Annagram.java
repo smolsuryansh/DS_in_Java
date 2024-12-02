f07ed8aba095f9e4e5a3dc0c9cc2d9b2bcb35a32
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-12-02 23:58:49 +0530

    neetcode 150, 1 and 2

---
+++ src/assignment_questions/Annagram.java
@@ -0,0 +1,24 @@
+package assignment_questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Annagram {
+
+    // neetcode 2
+    public boolean isAnnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> countS = new HashMap<>();
+        Map<Character, Integer> countT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return countS.equals(countT);
+    }
+}
---
+++ src/assignment_questions/ContainsDuplicate.java
@@ -0,0 +1,28 @@
+package assignment_questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+
+    // neetcode 1
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
