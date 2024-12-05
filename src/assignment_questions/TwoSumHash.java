8daad3600ea811a60aa07f573e994e759059bcd9
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-12-05 20:33:13 +0530

    neetcode 3, two sums using hash

---
+++ src/assignment_questions/TwoSumHash.java
@@ -0,0 +1,23 @@
+package assignment_questions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumHash {
+
+    // neetcode 3
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(num, i); // store the values to check in the future
+        }
+        return new int[]{};
+    }
+}
