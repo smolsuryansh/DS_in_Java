acaa728b10845208c2f8ff136882bf760f64c12e
Suryansh Dubey <suryanshdubey2525@gmail.com>
2024-12-08 15:40:04 +0530

    neetcode 5, leetcode 347, Top K elements (optimized)

--- src/assignment_questions/TopKElements.java
+++ src/assignment_questions/TopKElements.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class TopKElements {
 
     // neetcode 5, leetcode 347
-    public int[] topKFrequency(int[] nums, int k) {
+    public int[] topKFrequency1(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
 
         for (int num : nums) {
@@ -28,5 +28,35 @@ public class TopKElements {
             result[i] = arr.get(i)[1];
         }
         return result;
+    }
+
+    // optimized approach
+    public int[] topKFrequency(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int num : freq[i]) {
+                res[index++] = num;
+                if (index == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
     }
 }
